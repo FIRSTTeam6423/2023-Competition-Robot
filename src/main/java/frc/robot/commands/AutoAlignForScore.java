5ed4354bc50d14c1879084303837b4cbe2167a9b
dabeycorn <Dasun20202020@hotmail.com>
2023-10-19 11:40:48 -0700

    asdfjasdlkfjalsdk;fj

--- src/main/java/frc/robot/commands/AutoAlignForScore.java
+++ src/main/java/frc/robot/commands/AutoAlignForScore.java
@@ -5,11 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.DriveUtil;
 
 public class AutoAlignForScore extends CommandBase {
   /** Creates a new AutoAlignForScore. */
-  public AutoAlignForScore(DriveUtil du) {
+  public AutoAlignForScore() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
--- src/main/java/frc/robot/commands/TwoScoreAuto.java
+++ src/main/java/frc/robot/commands/TwoScoreAuto.java
@@ -6,42 +6,37 @@ package frc.robot.commands;
 
 import java.util.HashMap;
 
-import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ArmUtil;
 import frc.robot.subsystems.DriveUtil;
 import frc.robot.subsystems.GrabUtil;
 import frc.robot.util.ArmState;
-import frc.robot.Constants;
-import frc.robot.commands.AutoFollowTrajectorySwerve;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class TwoScoreAuto extends SequentialCommandGroup {
   /** Creates a new ToConeAuto. */
-  public TwoScoreAuto(DriveUtil du, GrabUtil gu, ArmUtil au) {
+  public TwoScoreAuto(PathPlannerTrajectory path, DriveUtil du, GrabUtil gu, ArmUtil au) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    HashMap<String, Command> eventMap = new HashMap<String, Command>();
+    HashMap<String, Command> eventMap=new HashMap<>();
     eventMap.put("spit", new SpitSeconds(gu, 3));
-    //System.out.println("EAGHHHHHH"+path.getMarkers());
-    // addCommands(
-    //   new SetArmState(au, ArmState.LOW),
-    //   new SpitSeconds(gu, 1),
-    //   new SetArmState(au, ArmState.RETRACT),
-    //   new AutoFollowTrajectorySwerve(du, path, new PathConstraints( Constants.ALIGN_TO_TAG_MAX_VELOCITY, Constants.ALIGN_TO_TAG_MAX_ACCELERATION)),
-    //   new SpitSeconds(gu, 2),//does something
-    //   //follows path back
-    //   new AutoFollowTrajectorySwerve(du, path, new PathConstraints(
-    //     Constants.ALIGN_TO_TAG_MAX_VELOCITY, Constants.ALIGN_TO_TAG_MAX_ACCELERATION)),
-    //   new AutoAlignForScore(du) 
-    // );
+    System.out.println("EAGHHHHHH"+path.getMarkers());
     addCommands(
-      new ExecutePathGroupWithEvents(du, "TwoScoreAuto", eventMap)
+      new SetArmState(au, ArmState.LOW),
+      new SpitSeconds(gu, 1),
+      new SetArmState(au, ArmState.RETRACT),
+      new FollowPathWithEvents(
+        new AutoFollowTrajectorySwerve(du, path),
+        path.getMarkers(),
+        eventMap
+      ),
+      new AutoAlignForScore(du)
     );
   }
 }
