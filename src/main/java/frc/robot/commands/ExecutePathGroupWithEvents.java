0fb7a2fbc70bff1ee4d7fbf3890080bc962dd239
Team6423 <55815254+Team6423@users.noreply.github.com>
2023-10-23 15:48:04 -0500

    New pathgroup command, not working yet.

---
+++ src/main/java/frc/robot/commands/ExecutePathGroupWithEvents.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveUtil;
+
+//follows a path with stop events not to be confused with the other one
+public class ExecutePathGroupWithEvents extends SequentialCommandGroup {
+  /** Creates a new ExecutePathGroupWithEvents. */
+  Command currentCommand;
+  List<PathPlannerTrajectory> pathGroup;
+  DriveUtil du;
+  public ExecutePathGroupWithEvents(DriveUtil du, String filename, HashMap<String, Command> eventMap) {
+    this.pathGroup= PathPlanner.loadPathGroup(
+      filename,
+      Constants.MAX_PATH_VELOCITY, 
+      Constants.MAX_PATH_ACCELERATION
+    );
+    this.du = du;
+    for(PathPlannerTrajectory traj : pathGroup){
+      addCommands(new AutoFollowTrajectorySwerve(du,traj));
+      for(String name : traj.getEndStopEvent().names){
+        Command c=eventMap.get(name);
+        if(c == null) addCommands(c);
+      }
+
+    }
+  }
+
+
+}
