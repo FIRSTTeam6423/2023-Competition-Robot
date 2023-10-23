176176acb0debe40984f574c90a2c88c927e57a0
Team6423 <55815254+Team6423@users.noreply.github.com>
2023-10-23 15:48:04 -0500

    New pathgroup command, not working yet.

--- src/main/java/frc/robot/commands/ExecutePathGroupWithEvents.java
+++ src/main/java/frc/robot/commands/ExecutePathGroupWithEvents.java
@@ -34,8 +34,7 @@ public class ExecutePathGroupWithEvents extends SequentialCommandGroup {
       addCommands(new AutoFollowTrajectorySwerve(du,traj));
       for(String name : traj.getEndStopEvent().names){
         Command c=eventMap.get(name);
-        if(c != null) addCommands(c);
-        else System.err.println("Command "+name+" not defined!");
+        if(c == null) addCommands(c);
       }
 
     }
