6b9dfaf58bfd64d522fb003e338cd265b19e74ea
Fred <42040138+freddxyz@users.noreply.github.com>
2024-01-09 15:52:35 -0500

    Made autofollowtrajectory a custom path follower
    AutoFollowTrajectorySwerve is changed to a single commnd instead of a sequentialcommandgroup, and it will now follow paths using custom code instead of using pathplannerlib swerve following commands.

--- src/main/java/frc/robot/commands/AutoFollowTrajectorySwerve.java
+++ src/main/java/frc/robot/commands/AutoFollowTrajectorySwerve.java
@@ -10,30 +10,78 @@ import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot;
 import frc.robot.subsystems.DriveUtil;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoFollowTrajectorySwerve extends SequentialCommandGroup {
+public class AutoFollowTrajectorySwerve extends CommandBase {
+	//NEEEDS TO BE RENAMED TO AUTOFOLLOWPATHGROUP SOON
   /** Creates a new AutoFollowTrajectorySwerve. */
-	public AutoFollowTrajectorySwerve(DriveUtil driveUtil, PathPlannerTrajectory traj) {
-	// Add your commands in the addCommands() call, e.g.
-	// addCommands(new FooCommand(), new BarCommand());
-		PIDController thetaController = new PIDController(.35, .035, 4.5);//new PIDController(4.5, 30, 5);
+  	private DriveUtil du;
+  	private PathPlannerTrajectory traj;
+
+	//controlelrs
+	PIDController xController;
+	PIDController yController;
+	PIDController omegaController;
+
+	private Timer timer = new Timer();
+	@Override
+ 	public void initialize() {
+		du.start();
+		timer.reset();
+		timer.start();
+		PathPlannerState pInitialState = (PathPlannerState)traj.sample(0);
+		Pose2d initialPose = new Pose2d(
+			traj.getInitialPose().getTranslation(),
+			pInitialState.holonomicRotation
+		);
+		System.out.println("END STATE THING: " + traj.getEndState().holonomicRotation);
+		du.resetPose(initialPose);
+  	}
+
+	public AutoFollowTrajectorySwerve(
+		DriveUtil du, 
+		PathPlannerTrajectory traj, 
+		PIDController xController, 
+		PIDController yController, 
+		PIDController omegaController
+	) {
+		this.du = du;
+		this.traj = traj;
+		this.xController=xController;
+		this.yController=yController;
+		this.omegaController=omegaController;
+	}
+
+	@Override
+	public void execute() {
+		PathPlannerState goal = (PathPlannerState) traj.sample(timer.get());
+		//Trajectory.State wpilibGoal = AllianceFlipUtil.apply(goal);
+        Rotation2d swerveRot;
+        //if (Robot.alliance == DriverStation.Alliance.Red) {
+        //    swerveRot = new Rotation2d(
+        //        -goal.holonomicRotation.getCos(),
+        ///        goal.holonomicRotation.getSin());
+        //} else {
+        //    swerveRot = goal.holonomicRotation;
+        //}
+	}
+
+	/*PIDController thetaController = new PIDController(.35, .035, 4.5);//new PIDController(4.5, 30, 5);
 		//thetaController.enableContinuousInput(-Math.PI, Math.PI);
 		addCommands(
 			new InstantCommand(()->{
-				driveUtil.start();
-				PathPlannerState pInitialState = (PathPlannerState)traj.sample(0);
-				Pose2d initialPose = new Pose2d(
-					traj.getInitialPose().getTranslation(),
-					pInitialState.holonomicRotation
-				);
-				System.out.println("END STATE THING: " + traj.getEndState().holonomicRotation);
-				driveUtil.resetPose(initialPose);
+				
 			}),
 			new PPSwerveControllerCommand(
             	traj, 
@@ -46,7 +94,5 @@ public class AutoFollowTrajectorySwerve extends SequentialCommandGroup {
             	false, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
             	driveUtil // Requires thuis drive subsystem
         	)
-		);
-
-	}
+		);*/
 }
