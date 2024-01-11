2dbcdbc13348488cc81a14f7fbe6298efd1df8c1
dabeycorn <dasun20202020@gmail.com>
2024-01-11 11:55:13 -0800

    Added some photonvision code

--- src/main/java/frc/robot/commands/LockOntoNote.java
+++ src/main/java/frc/robot/commands/LockOntoNote.java
@@ -4,20 +4,11 @@
 
 package frc.robot.commands;
 
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveUtil;
-import frc.robot.subsystems.SwerveModule;
 
 public class LockOntoNote extends CommandBase {
   /** Creates a new DriveRobot. */
